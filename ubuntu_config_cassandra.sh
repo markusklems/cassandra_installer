ead6c616e74451754765af105d45fbe65bfd745e
Markus Klems <markusklems@gmail.com>
2012-12-10 14:57:02 +0100

    Update ubuntu_config_cassandra.sh

--- ubuntu_config_cassandra.sh
+++ ubuntu_config_cassandra.sh
@@ -29,12 +29,12 @@ sudo sed -i -e "s|ulimit -l unlimited|#ulimit -n \"\$FD_LIMIT\"|" /etc/init.d/ca
 sudo sed -i -e "s|ulimit -n \"\$FD_LIMIT\"|#ulimit -n \"\$FD_LIMIT\"|" /etc/init.d/cassandra
 
 # Set access permission.
-sudo chown -R $myuser /etc/init.d/cassandra
-sudo chown -R $myuser /var/run/cassandra
-sudo chown -R $myuser /var/lib/cassandra
-sudo chown -R $myuser /var/log/cassandra
-sudo chown -R $myuser /etc/cassandra
-sudo chown -R $myuser /usr/share/cassandra
-sudo chown -R $myuser /usr/bin/cassandra-*
-sudo chown -R $myuser /usr/sbin/cassandra
-sudo chown -R $myuser /etc/default/cassandra
+sudo chown cassandra:cassandra /etc/init.d/cassandra
+sudo chown  cassandra:cassandra /var/run/cassandra
+sudo chown -R cassandra:cassandra /var/lib/cassandra
+sudo chown -R cassandra:cassandra /var/log/cassandra
+sudo chown -R cassandra:cassandra /etc/cassandra
+sudo chown -R cassandra:cassandra /usr/share/cassandra
+sudo chown cassandra:cassandra /usr/bin/cassandra-*
+sudo chown cassandra:cassandra /usr/sbin/cassandra
+sudo chown cassandra:cassandra /etc/default/cassandra
